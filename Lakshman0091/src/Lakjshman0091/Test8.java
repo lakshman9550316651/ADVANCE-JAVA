16bfa0ad2d6323f045c28fe0cf14b5d7e6406a23
Meghana <lakshmanpavan2306@gmail.com>
2023-02-27 09:37:28 +0530

    Noo summary
    Completed

---
+++ Lakshman0091/src/Lakjshman0091/Test8.java
@@ -0,0 +1,10 @@
+package Lakjshman0091;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Lakshman");
+	}
+
+}
---
+++ Lakshman0091/src/Lakjshman0091/exception.java
@@ -0,0 +1,15 @@
+package Lakjshman0091;
+
+public class exception {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		
+			System.out.println("Program Strats ");
+			//throw new ArthematicException("DivisionBy Zero");
+			throw new InterruptedException("Thread Exception");
+			
+	
+	}
+
+}
